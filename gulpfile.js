dcb9b5fe1ccd8184e81694ce3b16bc6db8aa400b
Kenta Miyazaki <0924wolf@gmail.com>
2024-03-05 12:21:19 +0900

    Add Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const gulp = require('gulp')
-const rename = require('gulp-rename')
 const ejs = require('gulp-ejs')
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename')
 const replace = require('gulp-replace')
 const plumber = require('gulp-plumber')
 const { series } = require('gulp')
 
-// ejsファイルをhtmlへコンパイル
+const EJSPath = ['./src/*.ejs', '!./src/_*.ejs']
+const SassPath = ['./src/**/*.scss', '!./src/**/_*.scss']
+
+// ejsコンパイル
 const EJSCompile = (done) => {
-  gulp.src(['./src/*.ejs', '!./src/_*.ejs'])
+  gulp.src(EJSPath)
     .pipe(plumber())
     .pipe(ejs())
     .pipe(rename({ extname: '.html' }))
@@ -16,10 +20,21 @@ const EJSCompile = (done) => {
     done()
   }
 
-// ファイル監視
-const watchFiles = (done) => {
-  gulp.watch(['./src/*.ejs', '!./src/_*.ejs'], EJSCompile)
+// sassコンパイル
+const SassCompile = (done) => {
+  gulp.src(SassPath)
+    .pipe(sass.sync({outputStyle: 'expanded'}))
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./dist'))
   done()
 }
 
-exports.default = series(EJSCompile, watchFiles)
+// ファイル監視
+const watchFiles = (done) => {
+  gulp.watch(EJSPath, EJSCompile)
+  gulp.watch(SassPath, series(SassCompile))
+  gulp.watch(['./src/**/_*.scss'], series(SassCompile))
+  done()
+}
+
+exports.default = series(watchFiles, EJSCompile, SassCompile)
